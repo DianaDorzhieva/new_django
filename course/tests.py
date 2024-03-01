004f39cb5472efdd129cbc285ce67d1ea9080130
diana <diana-safina-2017@mail.ru>
2024-03-01 21:14:45 +0500

    all

--- course/tests.py
+++ course/tests.py
@@ -2,18 +2,25 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from config import settings
 from config.settings import AUTH_USER_MODEL
+from users.models import User
 
 
 class CourseTestCase(APITestCase):
     def setUp(self) -> None:
-        pass
+        self.user = User.objects.create(
+            email="Diana@mail.ru",
+            FIO="Diana",
+            password="123",
+            pk=5
+        )
 
     def test_create_course(self):
         """Тест на создание курса
         (для проверки теста необходимо закомментировать права доступа)"""
         data = {
             "name": "Test",
-            "text": "Test"
+            "text": "Test",
+            "owner_id": self.user.pk
 
         }
         responce = self.client.post(
@@ -30,7 +37,7 @@ class CourseTestCase(APITestCase):
         """Тестирование вывода списка курсов"""
         responce = self.client.get(
             '/course/'
-            )
+        )
 
         self.assertEquals(
             responce.status_code,
